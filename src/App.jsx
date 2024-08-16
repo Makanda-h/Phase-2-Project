ca72dcac4a8dd9959ff015a2c7277ca5e9b51a5d
Hope <hopemwaura39@gmail.com>
2024-08-16 05:23:51 +0300

    deletions

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,14 @@
-import React, { useState, useContext, useEffect } from "react";
+import React from 'react';
+import Checkout from './components/Checkout';
 
-import GlobalStyles from "./GlobalStyles";
-import { NavBar, OverLay, MainContainer, ProductList } from "./AppStyles";
-
-import Context from "./store/Context";
-import Cart from "./components/Cart";
-import Product from "./components/Product";
-
-export default function App() {
-  const [isToggle, setToggle] = useState(false);
-  const context = useContext(Context);
-
-  useEffect(() => {
-    console.log(context);
-  }, [context]);
-
+function App() {
   return (
-    <>
-      <GlobalStyles />
-      <NavBar>
-        <Cart
-          isToggle={isToggle}
-          setToggle={setToggle}
-          carts={context.carts}
-          removeProductFromCart={context.removeProductFromCart}
-          clearCart={context.clearCart}
-        />
-      </NavBar>
-
-      <MainContainer>
-        {isToggle && <OverLay />}
-        <ProductList>
-          {context.products.map((p) => (
-            <Product
-              key={p.id}
-              id={p.id}
-              imageURL={p.imageURL}
-              price={p.price}
-              title={p.title}
-              addProductToCart={context.addProductToCart}
-            />
-          ))}
-        </ProductList>
-      </MainContainer>
-    </>
+    <div>
+      <NavBar />
+      <Cart>
+        <Checkout />
+      </Cart>
+    </div>
   );
 }
+export default App;
