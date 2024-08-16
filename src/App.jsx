a64bdcb409e91fa890f235f7b480ff657a420262
Hope <hopemwaura39@gmail.com>
2024-08-16 05:06:21 +0300

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
-import  React from 'react'
-import NavBar from './components/NavBar'
-import './App.css'
+import React, { useState, useContext, useEffect } from "react";
 
-function App() {
-  
-  
+import GlobalStyles from "./GlobalStyles";
+import { NavBar, OverLay, MainContainer, ProductList } from "./AppStyles";
+
+import Context from "./store/Context";
+import Cart from "./components/Cart";
+import Product from "./components/Product";
+
+export default function App() {
+  const [isToggle, setToggle] = useState(false);
+  const context = useContext(Context);
+
+  useEffect(() => {
+    console.log(context);
+  }, [context]);
+
   return (
     <>
-      <header>
-        <NavBar />
-      </header>
-    </>
-  )
-}
+      <GlobalStyles />
+      <NavBar>
+        <Cart
+          isToggle={isToggle}
+          setToggle={setToggle}
+          carts={context.carts}
+          removeProductFromCart={context.removeProductFromCart}
+          clearCart={context.clearCart}
+        />
+      </NavBar>
 
-export default App
+      <MainContainer>
+        {isToggle && <OverLay />}
+        <ProductList>
+          {context.products.map((p) => (
+            <Product
+              key={p.id}
+              id={p.id}
+              imageURL={p.imageURL}
+              price={p.price}
+              title={p.title}
+              addProductToCart={context.addProductToCart}
+            />
+          ))}
+        </ProductList>
+      </MainContainer>
+    </>
+  );
+}
