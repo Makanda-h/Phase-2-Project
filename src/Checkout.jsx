0d8b300a2ec47cf6a05390a1f2873241c8f30ae0
Hope <hopemwaura39@gmail.com>
2024-08-18 02:07:14 +0300

    corrections

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -1,16 +1,33 @@
 import React from "react";
+import swal from 'sweetalert';
 
 function Checkout({ cart }) {
   const totalAmount = cart.reduce(
     (total, item) => total + item.price * item.quantity,
     0
-  ); //calculate the total amount of the cart
+  ); 
+  //function to show alert
+  function showAlert(){
+    swal({
+      title: "Order Placed Successfully!!",
+      text: "Pay using Paybill 123456.",
+      icon: "success",
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        swal("Confirmed!", "Your order will be served in 5 minutes", "success");
+      }
+    });
+  }
+  
+  
+  //calculate the total amount of the cart
   return (
-    <div>
+    <div className="checkout-card">
       <h1>Checkout</h1>
       <div className="checkout-items">
         {cart.map((item) => (
-          <div key={item.id} className="checkout-item">
+          <div key={item.id} className="checkout-card">
             <h2>{item.name}</h2>
             <p>Price: {item.price}</p>
             <p>Subtotal: {item.price * item.quantity}</p>
@@ -19,11 +36,7 @@ function Checkout({ cart }) {
       </div>
       <h2>Total Amount: {totalAmount}</h2>
       <button
-        onClick={() =>
-          alert(
-            "Order placed successfully.For M-Pesa payments, use BUY GOODS:808080 .For cash payment , the bill will be presented to you once your order is processed . Have a nice time !"
-          )
-        }
+        onClick={showAlert}
       >
         Place Order
       </button>
--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,6 @@ function Home({ products, addToCart }) {
       <div className="product-list">
         {products.map(product => (
           <div key={product.id} className="product-card">
-            <img src={product.image} alt={product.name} />
             <h2>{product.name}</h2>
             <p>{product.description}</p>
             <p>Price: {product.price}</p>
